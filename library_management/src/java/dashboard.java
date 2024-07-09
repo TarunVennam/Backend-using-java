e17fc7ccc6454c1b2474ae417b83be21ea7608ce
22RP11161 <114095515+Diane0780078723@users.noreply.github.com>
2024-07-09 16:36:20 +0200

    Formative Assessment Learning Unit 4

---
+++ library_management/src/java/dashboard.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class dashboard extends HttpServlet {
+
+    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html"); 
+    PrintWriter out = response.getWriter(); 
+
+    HttpSession session = request.getSession(false); // Get existing session, do not create new
+    
+    if (session != null && "uname".equals("admin@12")) { 
+        String name = (String) session.getAttribute("uname"); 
+        out.print("Hello, " + name + ". Welcome to your profile."); 
+        request.getRequestDispatcher("dashboard.html").include(request, response); 
+    } 
+    else { 
+        out.print("<script>alert('Please login first.'); window.location.href='index.html';</script>");
+    } 
+    
+    out.close(); 
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
